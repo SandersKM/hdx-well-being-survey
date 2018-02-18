6b5dd25a1a69ad0a732012239b0e62532a1fe847
Kate Sanders <katesandersm@gmail.com>
2018-02-17 18:03:28 -0600

    Added Comments and started constructing mean and response rate functions

--- Analysis.R
+++ Analysis.R
@@ -1,3 +1,8 @@
-library(readxl)
+
+library(readxl) #library used to import Excel data
+# import the Hendrix Well Being Survey Data (change location for your own HWBS Data)
 dataset <- read_excel("C:/Users/kates/Desktop/HWBS_STUDENTS_2017_condensed.xlsx")
-dataset
+rownum <- 531 #total number of responses recorded
+mean(dataset$Overall_MH_R, na.rm=TRUE) #average mental health (scale:5)
+(rownum - sum(is.na(dataset$Overall_MH_R)))/ rownum #response rate
+
