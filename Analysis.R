f2f06df8570d8785711cfebf8f77091960ef9ef1
Kate Sanders <katesandersm@gmail.com>
2018-02-17 20:33:49 -0600

    Added Function for response rate

--- Analysis.R
+++ Analysis.R
@@ -3,6 +3,20 @@ library(readxl) #library used to import Excel data
 # import the Hendrix Well Being Survey Data (change location for your own HWBS Data)
 dataset <- read_excel("C:/Users/kates/Desktop/HWBS_STUDENTS_2017_condensed.xlsx")
 rownum <- 531 #total number of responses recorded
-mean(dataset$Overall_MH_R, na.rm=TRUE) #average mental health (scale:5)
-(rownum - sum(is.na(dataset$Overall_MH_R)))/ rownum #response rate
+
+#function for determining the response rate (% data that was not NA)
+response_rate <- function(col){
+  rr <- (rownum - sum(is.na(col)))/ rownum 
+  return(rr)
+}
+
+#overall mental health  (scale:1-5)
+mean(dataset$Overall_MH_R, na.rm=TRUE) 
+response_rate(dataset$Overall_MH_R)
+
+#“I see myself as a person with mental illness” (scale:1-6)
+mean(dataset$MI_identity_R, na.rm=TRUE) 
+response_rate(dataset$MI_identity_R)
+
+
 
