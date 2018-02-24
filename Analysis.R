d33f31b5b81ba976a514bbfb13ee3089bac65c76
Kate Sanders <katesandersm@gmail.com>
2018-02-24 16:20:29 -0600

    Created functions to identify and record "flourishing" students

--- Analysis.R
+++ Analysis.R
@@ -53,8 +53,120 @@ response_rate(dataset$HDX_MH_impact)
 boxplot(dataset$HDX_MH_impact) #the responses here are very symetrical 
 
 #################################################################################
-# 
+# Positive mental health
 #################################################################################
+
+# Mental Health Continuum – Short Form (scale:0-70)
+mean(dataset$MHCSF_total, na.rm=TRUE) 
+response_rate(dataset$MHCSF_total)
+boxplot(dataset$MHCSF_total)
+
+######
+#respondents who are considered “flourishing” 
+######
+
+#respondent must feel 1/3 well being symptoms 
+#"every day" or "almost everyday" (4 or 5)
+hedonic_wb <- function(i){
+  if (!is.na(dataset$MHCSF1[i])){
+    if(dataset$MHCSF1[i]==4 | dataset$MHCSF1[i]==5){        
+      return(TRUE)
+    }
+  }
+  if (!is.na(dataset$MHCSF2[i])){
+    if(dataset$MHCSF2[i]==4 | dataset$MHCSF2[i]==5){       
+        return(TRUE)
+    }
+  }
+  if (!is.na(dataset$MHCSF3[i])){
+    if(dataset$MHCSF3[i]==4 | dataset$MHCSF3[i]==5){
+      return(TRUE)
+    }
+  }
+  return(FALSE)
+}
+
+#respondent must feel 6/11 positive functioning symptoms 
+#"every day" or "almost everyday" (4 or 5) 
+positive_func <- function(i){
+  sym_count <- 0
+  if (!is.na(dataset$MHCSF4[i])){
+    if(dataset$MHCSF4[i]==4 | dataset$MHCSF4[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF5[i])){
+    if(dataset$MHCSF5[i]==4 | dataset$MHCSF5[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF6[i])){
+    if(dataset$MHCSF6[i]==4 | dataset$MHCSF6[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF7[i])){
+    if(dataset$MHCSF7[i]==4 | dataset$MHCSF7[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF8[i])){
+    if(dataset$MHCSF8[i]==4 | dataset$MHCSF8[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF9[i])){
+    if(dataset$MHCSF9[i]==4 | dataset$MHCSF9[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF10[i])){
+    if(dataset$MHCSF10[i]==4 | dataset$MHCSF10[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF11[i])){
+    if(dataset$MHCSF11[i]==4 | dataset$MHCSF11[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF12[i])){
+    if(dataset$MHCSF12[i]==4 | dataset$MHCSF12[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF13[i])){
+    if(dataset$MHCSF13[i]==4 | dataset$MHCSF13[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if (!is.na(dataset$MHCSF14[i])){
+    if(dataset$MHCSF14[i]==4 | dataset$MHCSF14[i]==5){
+      sym_count <- sym_count + 1
+    }
+  }
+  if(sym_count > 5){
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+#count the number of "flourishing" respondents
+flourishing_count <- 0
+#create a boolean column in dataset for "flourishing" students 
+dataset$flourishing <- FALSE #initializing data in the col
+
+for(i in 1:rownum){
+  #both functions must be true
+  if(hedonic_wb(i) & positive_func(i)){
+    dataset$flourishing[i] <- TRUE 
+    flourishing_count <- flourishing_count + 1
+  }
+}
+
+#percentage of respondents considered flourishing:
+flourishing_count/rownum
+
 
 #################################################################################
 # 
