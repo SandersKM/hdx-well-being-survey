598fa234ab75104e85375d0b1205f90f39992027
Kate Sanders <katesandersm@gmail.com>
2018-03-20 08:56:56 -0500

    Finished Sleep analysis

--- sleep.R
+++ sleep.R
@@ -1,3 +1,5 @@
+
+library(readxl) #library used to import Excel data
 # import the Hendrix Well Being Survey Data (change location for your own HWBS Data)
 dataset <- read_excel("C:/Users/kates/Desktop/HWBS_STUDENTS_2017_condensed.xlsx")
 rownum <- 531 #total number of responses recorded
@@ -13,27 +15,52 @@ response_rate <- function(col){
 #################################################################################
 
 # typical hours of sleep per night (open-ended)
-Hours_sleep_min <- min(dataset$Hours_sleep, na.rm=TRUE) #minimum
-Hours_sleep_max <- max(dataset$Hours_sleep, na.rm=TRUE) #maximum
-Hours_sleep_mean <- mean(dataset$Hours_sleep, na.rm=TRUE) 
-Hours_sleep_var <- var(dataset$Hours_sleep, na.rm = TRUE)
-Hours_sleep_std <- sqrt(Hours_sleep_var)
-Hours_sleep_rr <- response_rate(dataset$Hours_sleep)
+min(dataset$Hours_sleep, na.rm=TRUE) #minimum
+max(dataset$Hours_sleep, na.rm=TRUE) #maximum
+mean(dataset$Hours_sleep, na.rm=TRUE) 
+var(dataset$Hours_sleep, na.rm = TRUE)
+sqrt(Hours_sleep_var)
+response_rate(dataset$Hours_sleep)
 
 #Sleep Quality (Scale: 1=very good - 4=very bad)
-Sleep_quality_min <- min(dataset$Sleep_quality, na.rm=TRUE) #minimum
-Sleep_quality_max <- max(dataset$Sleep_quality, na.rm=TRUE) #maximum
-Sleep_quality_mean <- mean(dataset$Sleep_quality, na.rm=TRUE) 
-Sleep_quality_var <- var(dataset$Sleep_quality, na.rm = TRUE)
-Sleep_quality_std <- sqrt(Sleep_quality_var)
-Sleep_quality_rr <- response_rate(dataset$Sleep_quality)
+min(dataset$Sleep_quality, na.rm=TRUE) #minimum
+max(dataset$Sleep_quality, na.rm=TRUE) #maximum
+mean(dataset$Sleep_quality, na.rm=TRUE) 
+var(dataset$Sleep_quality, na.rm = TRUE)
+sqrt(Sleep_quality_var)
+response_rate(dataset$Sleep_quality)
 boxplot(dataset$Sleep_quality)
 
 #Sleep Hygiene (Scale: 14=very good - 70=very bad)
-SHI_total_min <- min(dataset$SHI_total, na.rm=TRUE) #minimum
-SHI_total_max <- max(dataset$SHI_total, na.rm=TRUE) #maximum
-SHI_total_mean <- mean(dataset$SHI_total, na.rm=TRUE) 
-SHI_total_var <- var(dataset$SHI_total, na.rm = TRUE)
-SHI_total_std <- sqrt(SHI_total_var)
-SHI_total_rr <- response_rate(dataset$SHI_total)
+min(dataset$SHI_total, na.rm=TRUE) #minimum
+max(dataset$SHI_total, na.rm=TRUE) #maximum
+mean(dataset$SHI_total, na.rm=TRUE) 
+var(dataset$SHI_total, na.rm = TRUE)
+sqrt(SHI_total_var)
+response_rate(dataset$SHI_total)
 boxplot(dataset$SHI_total)
+
+# Between total Sleep Hygiene and sleep quality
+cor(dataset$SHI_total, dataset$Sleep_quality, use="complete.obs", method="pearson")
+cov(dataset$SHI_total, dataset$Sleep_quality, use="complete.obs", method="pearson")
+
+# Between total Sleep Hygiene and feeling sleepy during the day
+cor(dataset$SHI_total, dataset$Sleep_hygiene15, use="complete.obs", method="pearson")
+cov(dataset$SHI_total, dataset$Sleep_hygiene15, use="complete.obs", method="pearson")
+
+# Between total Sleep Hygiene and worry about sleep
+cor(dataset$SHI_total, dataset$Sleep_hygiene16, use="complete.obs", method="pearson")
+cov(dataset$SHI_total, dataset$Sleep_hygiene16, use="complete.obs", method="pearson")
+
+# Between total Sleep Hygiene and feeling more moody now than before
+cor(dataset$SHI_total, dataset$Sleep_hygiene17, use="complete.obs", method="pearson")
+cov(dataset$SHI_total, dataset$Sleep_hygiene17, use="complete.obs", method="pearson")
+
+# Between total Sleep Hygiene and feeling like it takes more effort to get things done
+cor(dataset$SHI_total, dataset$Sleep_hygiene18, use="complete.obs", method="pearson")
+cov(dataset$SHI_total, dataset$Sleep_hygiene18, use="complete.obs", method="pearson")
+
+# Between total Sleep Hygiene and having more trouble paying attention and thinking than before
+cor(dataset$SHI_total, dataset$Sleep_hygiene19, use="complete.obs", method="pearson")
+cov(dataset$SHI_total, dataset$Sleep_hygiene19, use="complete.obs", method="pearson")
+
