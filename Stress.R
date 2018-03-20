5fdd3a61fdf08f7576dadef04fd95108a49ca2b5
Kate Sanders <katesandersm@gmail.com>
2018-03-19 22:23:36 -0500

    Got frequencies and Top 5 for Coping mechanisms

--- Stress.R
+++ Stress.R
@@ -7,6 +7,18 @@ library(readxl) #library used to import Excel data
 dataset <- read_excel("C:/Users/kates/Desktop/HWBS_STUDENTS_2017_condensed.xlsx")
 rownum <- 531 #total number of responses recorded. Change with new data!
 
+# Function for getting frequency of how many people checked each box
+# Erase "rownum" from return if you just want counts and not freqency.
+freq_bool <- function(category){
+  count <- 0
+  for(i in 1:rownum){
+    if (category[i] == 1){
+      count <- count + 1
+    }
+  }
+  return(count/rownum)
+}
+
 #################################################################################
 # Stress
 #################################################################################
@@ -26,67 +38,57 @@ Stress_total_var <- var(dataset$Stress_total, na.rm = TRUE)
 sqrt(Stress_total_var)
 response_rate(dataset$Stress_total)
 
-# Function for getting frequency of how many people checked each stressor
-# Erase "rownum" from return if you just want counts and not freqency.
-count_stressors <- function(category){
-  count <- 0
-  for(i in 1:rownum){
-    if (category[i] == 1){
-      count <- count + 1
-    }
-  }
-  return(count/rownum)
-}
+
 
 # Count how many ppl checked each kind of stressor
-Stressor_schoolfinances_count <- count_stressors(dataset$Stressor_schoolfinances)
-Stressor_schoolwork_count <-count_stressors(dataset$Stressor_schoolwork)
-Stressor_employment_count <-count_stressors(dataset$Stressor_employment)
-Stressor_romrelationships_count <-count_stressors(dataset$Stressor_romrelationships)
-Stressor_friendrelationships_count <-count_stressors(dataset$Stressor_friendrelationships)
-Stressor_familyrelationships_count <-count_stressors(dataset$Stressor_familyrelationships)
-Stressor_roommaterelationships_count <-count_stressors(dataset$Stressor_roommaterelationships)
-Stressor_illnessinjury_count <-count_stressors(dataset$Stressor_illnessinjury)
-Stressor_changelivingcond_count <-count_stressors(dataset$Stressor_changelivingcond)
-Stressor_professors_count <-count_stressors(dataset$Stressor_professors)
-Stressor_familypressure_count <-count_stressors(dataset$Stressor_familypressure)
-Stressor_familyresponsibilities_count <-count_stressors(dataset$Stressor_familyresponsibilities)
-Stressor_academicperf_count <-count_stressors(dataset$Stressor_academicperf)
-Stressor_academicplanning_count <-count_stressors(dataset$Stressor_academicplanning)
-Stressor_extracurriculars_count <-count_stressors(dataset$Stressor_extracurriculars)
-Stressor_timemange_count <-count_stressors(dataset$Stressor_timemange)
-Stressor_timeforself_count <-count_stressors(dataset$Stressor_timeforself)
-Stressor_future_count <-count_stressors(dataset$Stressor_future)
-Stressor_sleep_count <-count_stressors(dataset$Stressor_sleep)
-Stressor_appearance_count <-count_stressors(dataset$Stressor_appearance)
-Stressor_PH_count <-count_stressors(dataset$Stressor_PH)
-Stressor_MH_count <-count_stressors(dataset$Stressor_MH)
-Stressor_lonely_count <-count_stressors(dataset$Stressor_lonely)
-Stressor_social_count <-count_stressors(dataset$Stressor_social)
-Stressor_safety_count <-count_stressors(dataset$Stressor_safety)
-Stressor_SNS_count <-count_stressors(dataset$Stressor_SNS)
-Stressor_identity_count <-count_stressors(dataset$Stressor_identity)
-Stressor_politics_count <-count_stressors(dataset$Stressor_politics)
-Stressor_discrim_count <-count_stressors(dataset$Stressor_discrim)
-Stressor_supportingothersMH_count <-count_stressors(dataset$Stressor_supportingothersMH)
-Stressor_other_count <-count_stressors(dataset$Stressor_other)
+Stressor_schoolfinances_freq <- freq_bool(dataset$Stressor_schoolfinances)
+Stressor_schoolwork_freq <-freq_bool(dataset$Stressor_schoolwork)
+Stressor_employment_freq <-freq_bool(dataset$Stressor_employment)
+Stressor_romrelationships_freq <-freq_bool(dataset$Stressor_romrelationships)
+Stressor_friendrelationships_freq <-freq_bool(dataset$Stressor_friendrelationships)
+Stressor_familyrelationships_freq <-freq_bool(dataset$Stressor_familyrelationships)
+Stressor_roommaterelationships_freq <-freq_bool(dataset$Stressor_roommaterelationships)
+Stressor_illnessinjury_freq <-freq_bool(dataset$Stressor_illnessinjury)
+Stressor_changelivingcond_freq <-freq_bool(dataset$Stressor_changelivingcond)
+Stressor_professors_freq <-freq_bool(dataset$Stressor_professors)
+Stressor_familypressure_freq <-freq_bool(dataset$Stressor_familypressure)
+Stressor_familyresponsibilities_freq <-freq_bool(dataset$Stressor_familyresponsibilities)
+Stressor_academicperf_freq <-freq_bool(dataset$Stressor_academicperf)
+Stressor_academicplanning_freq <-freq_bool(dataset$Stressor_academicplanning)
+Stressor_extracurriculars_freq <-freq_bool(dataset$Stressor_extracurriculars)
+Stressor_timemange_freq <-freq_bool(dataset$Stressor_timemange)
+Stressor_timeforself_freq <-freq_bool(dataset$Stressor_timeforself)
+Stressor_future_freq <-freq_bool(dataset$Stressor_future)
+Stressor_sleep_freq <-freq_bool(dataset$Stressor_sleep)
+Stressor_appearance_freq <-freq_bool(dataset$Stressor_appearance)
+Stressor_PH_freq <-freq_bool(dataset$Stressor_PH)
+Stressor_MH_freq <-freq_bool(dataset$Stressor_MH)
+Stressor_lonely_freq <-freq_bool(dataset$Stressor_lonely)
+Stressor_social_freq <-freq_bool(dataset$Stressor_social)
+Stressor_safety_freq <-freq_bool(dataset$Stressor_safety)
+Stressor_SNS_freq <-freq_bool(dataset$Stressor_SNS)
+Stressor_identity_freq <-freq_bool(dataset$Stressor_identity)
+Stressor_politics_freq <-freq_bool(dataset$Stressor_politics)
+Stressor_discrim_freq <-freq_bool(dataset$Stressor_discrim)
+Stressor_supportingothersMH_freq <-freq_bool(dataset$Stressor_supportingothersMH)
+Stressor_other_freq <-freq_bool(dataset$Stressor_other)
 
 # put stressors in dataframe
-Stressors_all <- data.frame(Stressor_academicperf_count, Stressor_academicplanning_count, 
-                            Stressor_appearance_count, Stressor_changelivingcond_count, 
-                            Stressor_discrim_count, Stressor_employment_count, 
-                            Stressor_extracurriculars_count, Stressor_familypressure_count, 
-                            Stressor_familyrelationships_count, Stressor_future_count, 
-                            Stressor_familyresponsibilities_count,Stressor_romrelationships_count, 
-                            Stressor_friendrelationships_count, Stressor_identity_count, 
-                            Stressor_illnessinjury_count, Stressor_lonely_count, 
-                            Stressor_MH_count, Stressor_other_count, Stressor_PH_count,
-                            Stressor_politics_count, Stressor_professors_count, 
-                            Stressor_roommaterelationships_count, Stressor_safety_count, 
-                            Stressor_schoolfinances_count, Stressor_schoolwork_count,
-                            Stressor_sleep_count, Stressor_SNS_count, Stressor_social_count, 
-                            Stressor_supportingothersMH_count, Stressor_timeforself_count, 
-                            Stressor_timemange_count)
+Stressors_all <- data.frame(Stressor_academicperf_freq, Stressor_academicplanning_freq, 
+                            Stressor_appearance_freq, Stressor_changelivingcond_freq, 
+                            Stressor_discrim_freq, Stressor_employment_freq, 
+                            Stressor_extracurriculars_freq, Stressor_familypressure_freq, 
+                            Stressor_familyrelationships_freq, Stressor_future_freq, 
+                            Stressor_familyresponsibilities_freq,Stressor_romrelationships_freq, 
+                            Stressor_friendrelationships_freq, Stressor_identity_freq, 
+                            Stressor_illnessinjury_freq, Stressor_lonely_freq, 
+                            Stressor_MH_freq, Stressor_other_freq, Stressor_PH_freq,
+                            Stressor_politics_freq, Stressor_professors_freq, 
+                            Stressor_roommaterelationships_freq, Stressor_safety_freq, 
+                            Stressor_schoolfinances_freq, Stressor_schoolwork_freq,
+                            Stressor_sleep_freq, Stressor_SNS_freq, Stressor_social_freq, 
+                            Stressor_supportingothersMH_freq, Stressor_timeforself_freq, 
+                            Stressor_timemange_freq)
 
 # sort columns in order of highest stressors to lowest stressor.
 Stressors_all_sorted <- sort(Stressors_all, decreasing = TRUE)
@@ -161,3 +163,46 @@ cov(dataset$Overall_stress, dataset$PE_avg, use="complete.obs", method="pearson"
 #################################################################################
 # Coping with Stress
 #################################################################################
+
+Coping_relspirit_freq <- freq_bool(dataset$Coping_relspirit)
+Coping_Tvmovies_freq <- freq_bool(dataset$Coping_Tvmovies)
+Coping_internet_freq <- freq_bool(dataset$Coping_internet)
+Coping_read_freq <- freq_bool(dataset$Coping_read)
+Coping_friendsfamily_freq <- freq_bool(dataset$Coping_friendsfamily)
+Coping_outdoors_freq <- freq_bool(dataset$Coping_outdoors)
+Coping_sleep_freq <- freq_bool(dataset$Coping_sleep)
+Coping_hobby_freq <- freq_bool(dataset$Coping_hobby)
+Coping_caffeine_freq <- freq_bool(dataset$Coping_caffeine)
+Coping_cigarettes_freq <- freq_bool(dataset$Coping_cigarettes)
+Coping_marijuana_freq <- freq_bool(dataset$Coping_marijuana)
+Coping_illegaldrugs_freq <- freq_bool(dataset$Coping_illegaldrugs)
+Coping_food_freq <- freq_bool(dataset$Coping_food)
+Coping_videogames_freq <- freq_bool(dataset$Coping_videogames)
+Coping_sports_freq <- freq_bool(dataset$Coping_sports)
+Coping_meditateyoga_freq <- freq_bool(dataset$Coping_meditateyoga)
+Coping_Mhprofessional_freq <- freq_bool(dataset$Coping_Mhprofessional)
+Coping_writing_freq <- freq_bool(dataset$Coping_writing)
+Coping_planning_freq <- freq_bool(dataset$Coping_planning)
+Coping_talking_freq <- freq_bool(dataset$Coping_talking)
+Coping_other_freq <- freq_bool(dataset$Coping_other)
+Coping_exercise_freq <- freq_bool(dataset$Coping_exercise)
+Coping_music_freq <- freq_bool(dataset$Coping_music)
+Coping_alcohol_freq <- freq_bool(dataset$Coping_alcohol)
+Coping_medication_freq <- freq_bool(dataset$Coping_medication)
+
+Coping_all <- data.frame(Coping_Tvmovies_freq,Coping_internet_freq,Coping_read_freq,
+                         Coping_friendsfamily_freq,Coping_outdoors_freq, Coping_sleep_freq,
+                         Coping_hobby_freq,Coping_caffeine_freq,Coping_cigarettes_freq,
+                         Coping_marijuana_freq, Coping_illegaldrugs_freq,
+                         Coping_food_freq,Coping_videogames_freq,Coping_sports_freq,
+                         Coping_meditateyoga_freq,Coping_Mhprofessional_freq,
+                         Coping_writing_freq,Coping_planning_freq,Coping_talking_freq,
+                         Coping_other_freq,Coping_exercise_freq,Coping_music_freq,
+                         Coping_alcohol_freq,Coping_medication_freq,Coping_relspirit_freq)
+
+Coping_all_sorted <- sort(Coping_all, decreasing = TRUE)
+Coping_top_5 <- Coping_all_sorted[order(Coping_all_sorted, decreasing = TRUE)[1:5]]
+
+#percentage of people who meditate:
+Coping_meditateyoga_freq
+
